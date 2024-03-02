1bcc3eb4fd32e412114a5299a362ef7bc4d3584c
Ilea Cosmin Ionut <127050828+pierpatrat@users.noreply.github.com>
2024-03-02 17:02:37 +0200

    completed app2
    Completed app 2 but i added a auxiliary class to update the observers, i tried to update with the fir class but it did't work

---
+++ RTS/src/Lab2/Ex2/Main.java
@@ -0,0 +1,13 @@
+package Lab2.Ex2;
+public class Main {
+    private static final int noOfThreads=2;
+    private static final int processorLoad=1000000;
+    public static void main(String args[]){
+        Window win=new Window(noOfThreads);
+        for(int i =0; i<noOfThreads; i++){
+            Thread t = new Thread(new Fir(i, win, processorLoad));
+            t.setPriority(i%10 + 1);
+            t.start();
+        }
+    }
+}
