82cc025cf4c637e23d1bc937eadd31cbbff42cb9
Ilea Cosmin Ionut <127050828+pierpatrat@users.noreply.github.com>
2024-03-21 12:49:58 +0200

    Example 2 diagrams completed

--- RTS/src/Lab3/example3/JoinTestThread.java
+++ RTS/src/Lab3/example3/JoinTestThread.java
@@ -1,40 +1,38 @@
 package Lab3.example3;
 
-class JoinTestThread extends Thread{
+class JoinTestThread extends Thread {
+    private Thread t;
 
-    Thread t;
-    String n;
+    private int limit;
 
-    JoinTestThread(String n, Thread t){
-
+    JoinTestThread(String n, Thread t, int limit) {
         this.setName(n);
-
-        this.t=t;
-
+        this.t = t;
+        this.limit = limit;
     }
 
     public void run() {
-
-        System.out.println("Thread "+n+" has entered the run() method");
-
+        System.out.println(this.getName() + " has entered the run() method");
         try {
+            if (t != null)
+                t.join();
 
-            if (t != null) t.join();
+            int divisorSum = 0;
+            for (int i = 1; i <= limit; i++) {
+                if (limit % i == 0)
+                    divisorSum += i;
+            }
 
-            System.out.println("Thread "+n+" executing operation.");
+            Main.setSumOfDivisors(divisorSum);
 
+
+            System.out.println("Thread " + this.getName() + " executing operation.");
             Thread.sleep(3000);
+            System.out.println("Thread " + this.getName() + " has terminated operation.");
 
-            System.out.println("Thread "+n+" has terminated operation.");
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        catch(Exception e){e.printStackTrace();}
-
-    }
-
-    public void setT(Thread t) {
-        this.t = t;
     }
 
 }
---
+++ RTS/src/Lab3/example3/readme.md
@@ -0,0 +1,5 @@
+# ActivityDiagram
+![ActivityDiagram.png](diagram%2FActivityDiagram.png)
+
+# StateMachineDiagram
+![stateMachineDiagram.png](diagram%2FstateMachineDiagram.png)
